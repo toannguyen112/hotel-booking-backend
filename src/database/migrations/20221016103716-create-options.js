ae9b154d21a72070c4aec4a3f4a9963737774b5d
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-10-29 01:39:02 +0700

    Some bugs fixed

--- src/database/migrations/20221016103716-create-options.js
+++ src/database/migrations/20221016103716-create-options.js
@@ -3,20 +3,16 @@
 module.exports = {
   up: (queryInterface, Sequelize) =>
     queryInterface.createTable("options", {
-      file_id: {
+      id: {
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true,
         type: Sequelize.INTEGER,
-        references: {
-          model: "files",
-          key: "id",
-        },
       },
 
-      room_id: {
-        type: Sequelize.INTEGER,
-        references: {
-          model: "rooms",
-          key: "id",
-        },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
       },
     }),
 

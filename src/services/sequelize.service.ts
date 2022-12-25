07a1a67c593b79066293faa379130e695b6fccbd
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-12-25 23:41:14 +0700

    Better Code

--- src/services/sequelize.service.ts
+++ src/services/sequelize.service.ts
@@ -1,18 +1,20 @@
 import dotenv from "dotenv";
-
 import { Sequelize } from "sequelize-typescript";
 import path from "path";
 dotenv.config();
 
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+
 export default class SequelizeService {
   static async init() {
+
     try {
       let sequelize = new Sequelize({
-        database: "hotel-booking",
+        database: DB_NAME,
         dialect: "mysql",
-        username: "root",
-        password: "root",
-        host: "localhost",
+        username: DB_USER,
+        password: DB_PASS,
+        host: DB_HOST,
         define: {
           timestamps: false,
         },
@@ -26,6 +28,7 @@ export default class SequelizeService {
         .catch((error) => {
           console.error("Unable to connect to the database: ", error);
         });
+
 
       // init sequelize model
       sequelize.addModels([path.resolve(__dirname, `../models/`)]);

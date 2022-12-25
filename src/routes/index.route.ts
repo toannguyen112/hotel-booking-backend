83a8dbbb4c8f5c1ffd2db5ddc99139bedcccb8d2
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-12-25 20:47:35 +0700

    Better Code

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,12 +1,8 @@
-import { Router, Request, Response } from "express";
-import HelperController from "../controllers/helper.controller";
+import { Router } from "express";
 import HomeController from "../controllers/home.controller";
 
 const route: Router = Router();
 
 route.get("/", new HomeController().index);
-route.post("/model-data", (req: Request, res: Response) => {
-    return new HelperController().getModelData(req, res);
-});
 
 export default route;

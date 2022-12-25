6b05802a231a9febd959d7bfc403f887caeb3e2b
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-12-25 20:55:50 +0700

    Better Code

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  database: "hotel-booking",
-  dialect: "mysql",
-  username: "root",
-  password: "root",
-  host: "localhost",
-};
--- src/services/express.service.ts
+++ src/services/express.service.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from "express";
-
 import cors from "cors";
 import compression from "compression";
 import fs from "fs";

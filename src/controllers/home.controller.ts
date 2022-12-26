cc00d91c4360fd6af9ab67bced0f16f5ddf071f6
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-12-26 16:08:57 +0700

    Better Code

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import Tenant from "../models/tenant.model";
 export default class HomeController {
   async index(req: Request, res: Response) {
     return res.status(200).json('hotel-booking');

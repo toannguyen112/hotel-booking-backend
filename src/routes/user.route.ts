b5f27c9ea3d94101b0d4416918952c7a40c519e4
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-06-24 12:37:41 +0700

    Better Code

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -7,6 +7,7 @@ route.get("/user/get-users", new UserController().getUsers);
 route.get("/user/profile", auth, new UserController().profile);
 route.post("/user/login", new UserController().login);
 route.put("/user/update", auth, new UserController().update);
+route.delete("/user/delete/:id", new UserController().delete);
 route.post("/user/register", new UserController().register);
 route.get("/user/show/:id", new UserController().show);
 route.post("/user/logout", auth, new UserController().logout);

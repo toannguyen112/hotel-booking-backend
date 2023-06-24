8e726d07a49c594594fcb8f7ecc2b59b1d9f85f1
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-06-24 12:46:00 +0700

    Better Code

--- src/database/seeders/20220802094525-categories.js
+++ src/database/seeders/20220802094525-categories.js
@@ -5,32 +5,32 @@ module.exports = {
       {
         id: 1,
         name: "Phòng trọ, nhà trọ",
-        image: "https://pesweb.azureedge.net/spimg/hotelbannerimages/pestana-cr7-hotels/cr7-lisboa-building.jpg?scale=downscaleonly&encoder=freeimage&progressive=true&quality=50&w=1440&h=780&mode=crop&anchor=bottomcenter"
+        image: "https://www.edisonhotelnyc.com/resourcefiles/homeimages/edison-hero-image-facade.jpg?version=9142022133607"
       },
       {
         id: 2,
         name: "Nhà thuê nguyên căn",
-        image: "https://pesweb.azureedge.net/spimg/hotelbannerimages/pestana-cr7-hotels/cr7-lisboa-building.jpg?scale=downscaleonly&encoder=freeimage&progressive=true&quality=50&w=1440&h=780&mode=crop&anchor=bottomcenter"
+        image: "https://www.edisonhotelnyc.com/resourcefiles/homeimages/edison-hero-image-facade.jpg?version=9142022133607"
       },
       {
         id: 3,
         name: "Cho thuê căn hộ",
-        image: "https://pesweb.azureedge.net/spimg/hotelbannerimages/pestana-cr7-hotels/cr7-lisboa-building.jpg?scale=downscaleonly&encoder=freeimage&progressive=true&quality=50&w=1440&h=780&mode=crop&anchor=bottomcenter"
+        image: "https://www.edisonhotelnyc.com/resourcefiles/homeimages/edison-hero-image-facade.jpg?version=9142022133607"
       },
       {
         id: 4,
         name: "Cho thuê căn hộ mini",
-        image: "https://pesweb.azureedge.net/spimg/hotelbannerimages/pestana-cr7-hotels/cr7-lisboa-building.jpg?scale=downscaleonly&encoder=freeimage&progressive=true&quality=50&w=1440&h=780&mode=crop&anchor=bottomcenter"
+        image: "https://www.edisonhotelnyc.com/resourcefiles/homeimages/edison-hero-image-facade.jpg?version=9142022133607"
       },
       {
         id: 5,
         name: "Tìm người ở ghép",
-        image: "https://pesweb.azureedge.net/spimg/hotelbannerimages/pestana-cr7-hotels/cr7-lisboa-building.jpg?scale=downscaleonly&encoder=freeimage&progressive=true&quality=50&w=1440&h=780&mode=crop&anchor=bottomcenter"
+        image: "https://www.edisonhotelnyc.com/resourcefiles/homeimages/edison-hero-image-facade.jpg?version=9142022133607"
       },
       {
         id: 6,
         name: "Cho thuê mặt bằng",
-        image: "https://pesweb.azureedge.net/spimg/hotelbannerimages/pestana-cr7-hotels/cr7-lisboa-building.jpg?scale=downscaleonly&encoder=freeimage&progressive=true&quality=50&w=1440&h=780&mode=crop&anchor=bottomcenter"
+        image: "https://www.edisonhotelnyc.com/resourcefiles/homeimages/edison-hero-image-facade.jpg?version=9142022133607"
       },
     ]);
   },

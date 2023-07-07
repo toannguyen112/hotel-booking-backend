3d2e874346d2a806433ca3ba3981bf2070413011
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2023-07-08 00:45:56 +0700

    Better Code

--- src/database/seeders/20220802094528-permissions.js
+++ src/database/seeders/20220802094528-permissions.js
@@ -5,30 +5,37 @@ module.exports = {
       {
         id: 1,
         perm_name: `dashboard`,
+        perm_description: "Thống kê"
       },
       {
         id: 2,
         perm_name: `rooms`,
+        perm_description: "Phòng"
       },
       {
         id: 3,
         perm_name: `tenants`,
+        perm_description: "Người thuê"
       },
       {
         id: 4,
         perm_name: `users`,
+        perm_description: "Người dùng"
       },
       {
         id: 5,
         perm_name: `accounts`,
+        perm_description: "Tài khoản"
       },
       {
         id: 6,
         perm_name: `roles`,
+        perm_description: "Vai trò"
       },
       {
         id: 7,
         perm_name: `maps`,
+        perm_description: "Bản đồ"
       },
     ]);
   },
